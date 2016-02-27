b35259d8a784a0861407a3bc7aa3e78212e24257
charlotte vernier <charlotte@mypeplum.com>
2016-02-27 17:45:02 +0000

    first version

---
+++ main.js
@@ -0,0 +1,19 @@
+var histogramDiv = document.getElementById("histogram");
+
+var displayHistogram = function(data, element) {
+  var values = data.split(",");
+  for (var i = 0; i < values.length; i++) {
+    var bar = document.createElement("div");
+    bar.style.width = values[i] + "px";
+    element.appendChild(bar);
+  }
+
+}
+
+var dataFromHash = function() {
+  return document.location.hash.substring(1);
+}
+
+
+displayHistogram(dataFromHash(), histogramDiv);
+
