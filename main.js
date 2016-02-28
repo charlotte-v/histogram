934a67fd19f975c9bd6aa9d3a5a8a60da77e13ed
charlotte vernier <charlotte@mypeplum.com>
2016-02-28 17:08:26 +0000

    improve histogram style

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var displayHistogram = function(data, element) {
   var values = data.split(",");
   for (var i = 0; i < values.length; i++) {
     var bar = document.createElement("div");
+    bar.title = values[i];
     var max = Math.max.apply(null, values);
     bar.style.height = values[i] * 100 / max + "%";
     bar.style.width = 100 / values.length + "%";
